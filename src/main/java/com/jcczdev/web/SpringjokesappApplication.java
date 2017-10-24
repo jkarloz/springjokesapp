456548af7e98f06b16c8a436bad8644049f6829f
cancerberosoft1 <crisjherico@gmail.com>
2017-10-23 20:45:07 -0500

    Change code to use XML based Spring configuration instead of Java Configuration

--- src/main/java/com/jcczdev/web/SpringjokesappApplication.java
+++ src/main/java/com/jcczdev/web/SpringjokesappApplication.java
@@ -2,8 +2,10 @@ package com.jcczdev.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SpringjokesappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/jcczdev/web/config/ChuckConfiguration.java
+++ src/main/java/com/jcczdev/web/config/ChuckConfiguration.java
@@ -9,14 +9,16 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
  * @author Juan Carlos - 2017/10/23
  *
  */
-@Configuration
+// Configuration and Bean annotation were commented because we are implementing XML based configuration
+// This is made in chuck-config.xml file and SpringjokesappApplication.java
+//@Configuration
 public class ChuckConfiguration {
 	
 	/*
 	 * For this case, as ChuckNorrisQuotes is a class from an external jar and we need an instance of it
 	 * we make this java configuration.
 	 */
-	@Bean
+	//@Bean
 	public ChuckNorrisQuotes chuckNorrisQuotes() {
 		return new ChuckNorrisQuotes();
 	}
